9224c3e417a15e80d7b42d1e9c73028867008f7a
Alejandro Rubio <arubiodeveloper@gmail.com>
2020-01-13 12:53:57 -0700

    Convert formula into tokens and create stacks

--- Spreadsheet/FormulaEvaluator/Evaluator.cs
+++ Spreadsheet/FormulaEvaluator/Evaluator.cs
@@ -1,8 +1,31 @@
 ﻿using System;
+using System.Collections;
+using System.Text.RegularExpressions;
 
 namespace FormulaEvaluator
 {
-    public class Evaluator
+    public static class Evaluator
     {
+        public delegate int Lookup(String variable_name);
+
+        public static int Evaluate(String expression, Lookup variableEvaluator)
+        {
+            int result = 0;
+            string[] substrings = Regex.Split(expression, "(\\()|(\\))|(-)|(\\+)|(\\*)|(/)");
+            for(int i = 0; i < substrings.Length; i++)
+            {
+                substrings[i]=substrings[i].Trim();
+                Console.WriteLine(substrings[i]);
+            }
+
+            Stack opStack = new Stack();
+            Stack valStack = new Stack();
+
+            for (int j = 0; j < substrings.Length; j++)
+            {
+
+            }
+            return result;
+        }
     }
 }
--- Spreadsheet/Test_The_Evaluator_Console_App/Program.cs
+++ Spreadsheet/Test_The_Evaluator_Console_App/Program.cs
@@ -1,12 +1,15 @@
-﻿using System;
+﻿using FormulaEvaluator;
+using System;
 
 namespace Test_The_Evaluator_Console_App
 {
     class Program
     {
+        private static Evaluator.Lookup a; //borrar
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (Evaluator.Evaluate("5+5", a) != 10) Console.WriteLine("Error");
         }
     }
 }

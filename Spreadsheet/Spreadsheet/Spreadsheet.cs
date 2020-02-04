113ce532e5de0e87d9c4493762f8c994e28b2fd2
arubiodev <arubiodeveloper@gmail.com>
2020-02-03 23:20:14 -0700

    Starter files

---
+++ Spreadsheet/Spreadsheet/Spreadsheet.cs
@@ -0,0 +1,42 @@
+﻿using SpreadsheetUtilities;
+using SS;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Spreadsheet
+{
+    public class Spreadsheet : AbstractSpreadsheet
+    {
+        public override object GetCellContents(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IEnumerable<string> GetNamesOfAllNonemptyCells()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IList<string> SetCellContents(string name, double number)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IList<string> SetCellContents(string name, string text)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IList<string> SetCellContents(string name, Formula formula)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override IEnumerable<string> GetDirectDependents(string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}

0375bb441e21703f6b59af67304e7f8b2e4cafce
arubiodev <arubiodeveloper@gmail.com>
2020-02-09 14:43:19 -0700

    Added READMEs

--- Spreadsheet/Spreadsheet/Spreadsheet.cs
+++ Spreadsheet/Spreadsheet/Spreadsheet.cs
@@ -1,4 +1,16 @@
-﻿using SpreadsheetUtilities;
+﻿/// <summary> 
+/// Author:    Alejandro Rubio
+/// Partner:   None
+/// Date:      2/9/2020
+/// Course:    CS 3500, University of Utah, School of Computing 
+/// Copyright: CS 3500 and Alejandro Rubio - This work may not be copied for use in Academic Coursework. 
+/// 
+/// I, Alejandro Rubio, certify that I wrote this code from scratch and did not copy it in part or whole from  
+/// another source.  All references used in the completion of the assignment are cited in my README file. 
+/// 
+/// This file creates a Spreadsheet using the Formula and DependendcyGraph Library
+/// </summary>
+using SpreadsheetUtilities;
 using SS;
 using System;
 using System.Collections.Generic;
--- Spreadsheet/SpreadsheetTests/SpreadsheetTests.cs
+++ Spreadsheet/SpreadsheetTests/SpreadsheetTests.cs
@@ -1,3 +1,15 @@
+/// <summary> 
+/// Author:    Alejandro Rubio
+/// Partner:   None
+/// Date:      2/9/2020
+/// Course:    CS 3500, University of Utah, School of Computing 
+/// Copyright: CS 3500 and Alejandro Rubio - This work may not be copied for use in Academic Coursework. 
+/// 
+/// I, Alejandro Rubio, certify that I wrote this code from scratch and did not copy it in part or whole from  
+/// another source.  All references used in the completion of the assignment are cited in my README file. 
+/// 
+/// This file tests the Spreadsheet Library that uses DependencyGraph and Formula
+/// </summary>
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SS;
 using System;

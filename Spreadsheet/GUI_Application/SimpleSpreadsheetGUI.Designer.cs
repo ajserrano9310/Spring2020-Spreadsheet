1b062a423ad6d01f49f134a5e29533442b711d2b
Alejandro <Alejandro@DESKTOP-0DN8VSC>
2020-02-21 12:13:14 -0700

    Added GUI Application files and SpreadsheetGridWidget.

---
+++ Spreadsheet/GUI_Application/SimpleSpreadsheetGUI.Designer.cs
@@ -0,0 +1,206 @@
+﻿/// <summary>
+///   Original Author: Joe Zachary
+///   Further Authors: H. James de St. Germain
+///   
+///   Dates          : 2012-ish - Original 
+///                    2020     - Updated for use with ASP Core
+///                    
+///   This code represents a Windows Form element for a Spreadsheet
+///   
+///   This code is the "auto-generated" portion of the SimpleSpreadsheetGUI.
+///   See the SimpleSpreadsheetGUI.cs for "hand-written" code.
+///  
+/// </summary>
+
+using SpreadsheetGrid_Framework;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace CS3500_Spreadsheet_GUI_Example
+{
+    partial class SimpleSpreadsheetGUI
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.MainControlArea = new System.Windows.Forms.FlowLayoutPanel();
+            this.grid_widget = new SpreadsheetGrid_Framework.SpreadsheetGridWidget();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.sample_button = new System.Windows.Forms.Button();
+            this.sample_checkbox = new System.Windows.Forms.CheckBox();
+            this.sample_textbox = new System.Windows.Forms.TextBox();
+            this.menuStrip.SuspendLayout();
+            this.MainControlArea.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(584, 24);
+            this.menuStrip.TabIndex = 1;
+            this.menuStrip.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
+            this.closeToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.newToolStripMenuItem.Text = "New";
+            this.newToolStripMenuItem.Click += new System.EventHandler(this.NewToolStripMenuItem_Click);
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.closeToolStripMenuItem.Text = "Close";
+            this.closeToolStripMenuItem.Click += new System.EventHandler(this.CloseToolStripMenuItem_Click);
+            // 
+            // MainControlArea
+            // 
+            this.MainControlArea.AutoSize = true;
+            this.MainControlArea.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.MainControlArea.BackColor = System.Drawing.Color.Coral;
+            this.MainControlArea.Controls.Add(this.sample_button);
+            this.MainControlArea.Controls.Add(this.sample_checkbox);
+            this.MainControlArea.Controls.Add(this.sample_textbox);
+            this.MainControlArea.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.MainControlArea.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.MainControlArea.Location = new System.Drawing.Point(3, 3);
+            this.MainControlArea.MinimumSize = new System.Drawing.Size(100, 100);
+            this.MainControlArea.Name = "MainControlArea";
+            this.MainControlArea.Size = new System.Drawing.Size(578, 100);
+            this.MainControlArea.TabIndex = 4;
+            // 
+            // grid_widget
+            // 
+            this.grid_widget.AutoSize = true;
+            this.grid_widget.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.grid_widget.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.grid_widget.Location = new System.Drawing.Point(3, 103);
+            this.grid_widget.MaximumSize = new System.Drawing.Size(2100, 2000);
+            this.grid_widget.Name = "grid_widget";
+            this.grid_widget.Size = new System.Drawing.Size(578, 231);
+            this.grid_widget.TabIndex = 0;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.AutoSize = true;
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.MainControlArea, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.grid_widget, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 24);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(584, 337);
+            this.tableLayoutPanel1.TabIndex = 6;
+            // 
+            // sample_button
+            // 
+            this.sample_button.Location = new System.Drawing.Point(3, 3);
+            this.sample_button.Name = "sample_button";
+            this.sample_button.Size = new System.Drawing.Size(75, 23);
+            this.sample_button.TabIndex = 0;
+            this.sample_button.Text = "button1";
+            this.sample_button.UseVisualStyleBackColor = true;
+            this.sample_button.Click += new System.EventHandler(this.sample_button_Click);
+            // 
+            // sample_checkbox
+            // 
+            this.sample_checkbox.AutoSize = true;
+            this.sample_checkbox.Location = new System.Drawing.Point(84, 3);
+            this.sample_checkbox.Name = "sample_checkbox";
+            this.sample_checkbox.Size = new System.Drawing.Size(80, 17);
+            this.sample_checkbox.TabIndex = 1;
+            this.sample_checkbox.Text = "checkBox1";
+            this.sample_checkbox.UseVisualStyleBackColor = true;
+            this.sample_checkbox.CheckedChanged += new System.EventHandler(this.sample_checkbox_CheckedChanged);
+            // 
+            // sample_textbox
+            // 
+            this.sample_textbox.Location = new System.Drawing.Point(170, 3);
+            this.sample_textbox.Name = "sample_textbox";
+            this.sample_textbox.Size = new System.Drawing.Size(100, 20);
+            this.sample_textbox.TabIndex = 2;
+            this.sample_textbox.TextChanged += new System.EventHandler(this.sample_textbox_TextChanged);
+            // 
+            // SimpleSpreadsheetGUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 361);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.menuStrip);
+            this.MainMenuStrip = this.menuStrip;
+            this.Name = "SimpleSpreadsheetGUI";
+            this.Text = "Sample GUI - Copy/Modify/Profit";
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.MainControlArea.ResumeLayout(false);
+            this.MainControlArea.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        
+        private SpreadsheetGridWidget grid_widget;
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+
+        private FlowLayoutPanel MainControlArea;
+        private TableLayoutPanel tableLayoutPanel1;
+        private Button sample_button;
+        private CheckBox sample_checkbox;
+        private TextBox sample_textbox;
+    }
+}
+
